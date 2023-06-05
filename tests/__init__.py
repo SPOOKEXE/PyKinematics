428a7feced54372458cb6ded2344494b062e5ebd
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2023-06-05 12:53:49 +0930

    cleanup

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,58 @@
 
-from forward_ik import run_2d_test as forward_2d_test
-from inverse_ik import run_2d_test as inverse_2d_test
+from math import floor, pi, cos, sin
+from utility import DrawApp, run_draw_widget
+
+from os import path as os_path
+from sys import path as sys_path
+
+FILE_DIRECTORY = os_path.dirname(os_path.realpath(__file__))
+
+sys_path.append( os_path.join(FILE_DIRECTORY, "..") )
+
+from src.fmath.Vector2 import Vector2
+from src.objects.Segment2D import Segment2D, Tentacle2D
+
+sys_path.pop()
+
+def run_2d_test():
+	WIDGET_SIZE = (800, 800)
+
+	TOTAL_SEGMENTS = 3
+	SEGMENT_LENGTHS = [150, 100, 50]
+
+	#segment_colors = ['white']
+	segment_colors = ["red", "blue", "orange", "white"]
+	anchorPoint = Vector2(WIDGET_SIZE[0] / 2, WIDGET_SIZE[1])
+
+	tentacle = Tentacle2D(TOTAL_SEGMENTS, SEGMENT_LENGTHS)
+	tentacle.BASE_VECTOR = anchorPoint
+
+	def pre_update(self : DrawApp):
+		nonlocal tentacle
+		mx, my = self.get_mouse_xy()
+		tentacle.follow(mx, my)
+
+	def post_update(self : DrawApp):
+		pass
+
+	def pre_draw(self : DrawApp):
+		nonlocal tentacle
+		self.clear_screen()
+
+		length = len(tentacle.SEGMENTS)
+		for index in range(length):
+			segment = tentacle.SEGMENTS[index]
+			self.draw_line(
+				segment.a.x, segment.a.y,
+				segment.b.x, segment.b.y,
+				fill=segment_colors[index%len(segment_colors)],
+				width=max(8 - floor((index * 12) / length), 2)
+			)
+
+	def post_draw(self : DrawApp):
+		pass
+
+	run_draw_widget(PRE_UPDATE=pre_update, POST_UPDATE=post_update, PRE_DRAW=pre_draw, POST_DRAW=post_draw, WIDGET_SIZE=WIDGET_SIZE)
 
 if __name__ == '__main__':
-	forward_2d_test()
-	inverse_2d_test()
+	run_2d_test()
